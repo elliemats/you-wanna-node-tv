ea05941accdfbe82b7b0caf312a1be8785614460
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-11-13 10:25:58 -0600

    Add 'handleInput' function to modify user input if show title has multiple words.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var userInput = process.argv.slice(2);
-
 var shows = require('./show')
+
+function handleInput(input) {
+  if(input.length > 1) return input.join("-"); }
+
+var userInput = handleInput(process.argv.slice(2));
+
 shows.getData( userInput );
+
+
+
+
